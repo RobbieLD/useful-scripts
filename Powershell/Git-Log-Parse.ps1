b9546b9dc53fc6347d369cf762b1c1f13632c4a1
Rob Davis <robd@youi.com>
2022-04-20 15:49:33 +1000

    Fixing issue with git log parser

--- Powershell/Git-Log-Parse.ps1
+++ Powershell/Git-Log-Parse.ps1
@@ -1,11 +1,11 @@
 
-$messages = git log master..develop --grep='dataprep' --pretty=format:"%b" | ConvertFrom-Json
+$messages = "{`"root`":[$(git log master..develop --grep='dataprep' --pretty=format:"%b,")]}" | ConvertFrom-Json
 
-Write-Host "# Data Prep"
-Write-Host
-Write-Host  "| Class | Field | Data Type |"
-Write-Host  "| ----- | ----- | ----------|"
+Write-Output -InputObject "# Data Prep"
+Write-Output -InputObject ""
+Write-Output -InputObject "| Class | Field | Data Type |"
+Write-Output -InputObject  "| ----- | ----- | ----------|"
 
-foreach ($message in $messages) {
-    Write-Host "|$($message.dataprep.class) | $($message.dataprep.field) | $($message.dataprep.type) |"
+foreach ($message in $messages.root) {
+    Write-Output -InputObject "|$($message.dataprep.class) | $($message.dataprep.field) | $($message.dataprep.type) |"
 }
